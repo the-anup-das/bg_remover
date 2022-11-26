59baad6bf67c939f849a8a64ead20eaf46d81255
the-anup-das <anup.das17@yahoo.com>
2022-11-27 02:36:13 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -2,19 +2,18 @@ import streamlit as st
 import cv2
 import numpy as np
 
-img_file_buffer = st.camera_input("Take a picture")
+st.set_page_config(page_title = 'Remove Background')
 
-# @st.cache(suppress_st_warning=True)
+st.title("Remove Background")
 
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+menu = ['Webcam','Select Image','About']
+choice = st.sidebar.selectbox('Menu', menu)
 
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
+if choice == 'Webcam':
+    st.subheader("Capture a Image using webcam")
 
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
+elif choice == 'Select Image':
+    st.subheader("Upload Your Image")
+
+elif choice == 'About':
+    st.subheader("About page")
