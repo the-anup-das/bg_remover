a282f4fdfeeee5360e0a7c85c88af5c0731bc754
the-anup-das <anup.das17@yahoo.com>
2022-11-27 13:14:44 +0530

    adding images

--- app/main.py
+++ app/main.py
@@ -1,19 +1,68 @@
 import streamlit as st
 import cv2
 import numpy as np
+from utils import webcam, upload_image
+from PIL import Image, ImageEnhance
+import os
 
-st.set_page_config(page_title = 'Remove Background')
+def main():
+    st.set_page_config(page_title = 'Remove Background')
 
-st.title("Remove Background")
+    st.title("Remove Background")
 
-menu = ['Webcam','Select Image','About']
-choice = st.sidebar.selectbox('Menu', menu)
+    path = os.path.dirname(__file__)
+    icon = path+'\\assets\\icon.png'
+    print(icon)
 
-if choice == 'Webcam':
-    st.subheader("Capture a Image using webcam")
+    image = Image.open(icon)
 
-elif choice == 'Select Image':
-    st.subheader("Upload Your Image")
+    # Create two columns with different with
+    col1, col2 = st.columns([0.8, 0.2])
+    
+    #display header text using css style
+    with col1:
+        st.markdown("""
+        <style>.font {
+            font-size:35px; font-family:'Cooper Black'; color:#FF9633;
+        }
+        </style>
+        """, unsafe_allow_html = True)
+        st.markdown('<p class="font">Upload your photo here.</p>', unsafe_allow_html = True)
+    
+    with col2:
+        st.image(image, width=150)
 
-elif choice == 'About':
-    st.subheader("About page")
+    menu = ['Select Image','Webcam','About']
+    choice = st.sidebar.selectbox('Menu', menu)
+
+    if choice == 'Select Image':
+        st.subheader("Upload Your Image")
+        uploaded_img = upload_image()
+        
+        if st.button("Remove Background"):
+            # st.write(my_text)
+            # download = FileDownloader(uploaded_img).download()
+            # with open("flower.png", "rb") as file:
+            btn = st.download_button(
+                        label="Download Output",
+                        data=uploaded_img,
+                        file_name=uploaded_img.name,
+                        mime="image/png"
+                    )
+
+    elif choice == 'Webcam':
+        st.subheader("Capture a Image using webcam")
+        uploaded_img = webcam()
+
+        btn = st.download_button(
+                        label="Download Output",
+                        data=uploaded_img,
+                        file_name=uploaded_img.name,
+                        mime="image/png"
+                    )
+
+    elif choice == 'About':
+        st.subheader("About page")
+
+if __name__ == '__main__':
+    main()
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import cv2
 import numpy as np
+from PIL import Image, ImageEnhance
+
+import streamlit.components as stc
+import base64
+import time
+
+# class FileDownloader(object):
+	
+# 	def __init__(self, data,filename='myfile',file_ext='png'):
+# 		super(FileDownloader, self).__init__()
+# 		self.data = data
+# 		self.filename = filename
+# 		self.file_ext = file_ext
+
+# 	def download(self):
+# 		b64 = base64.b64encode(self.data.encode()).decode()
+# 		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
+# 		st.markdown("#### Download File ###")
+# 		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here!!</a>'
+# 		st.markdown(href,unsafe_allow_html=True)
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
 
 def webcam():
     img_file_buffer = st.camera_input("Take a picture")
@@ -19,4 +43,26 @@ def webcam():
         # Check the shape of cv2_img:
         # Should output shape: (height, width, channels)
         # st.write(cv2_img.shape)
+        
+        st.image(load_image(img_file_buffer), width = 250)
         return cv2_img
+
+def upload_image():
+    st.subheader("Image")
+    image_file = st.file_uploader("Upload_file", type = ["png", "jpg", "jpeg"])
+
+    if image_file is not None:
+        file_details = {
+            'filename': image_file.name,
+            'filetype': image_file.type,
+            'filesize': image_file.size
+        }
+
+        st.write(file_details)
+
+        st.image(load_image(image_file), width = 250)
+
+        # image = Image.open(image_file)
+
+        return image_file
+
